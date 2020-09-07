8d16e64d108806c5e9b7949c7befda65584a3fd7
Unique Bhattarai <uniqgaming26@gmail.com>
2020-09-07 22:23:51 +0545

    done for today
    This much done for today There is a error on deleting and completing the list
    will do tomorrow

--- src/Component/todolist.js
+++ src/Component/todolist.js
@@ -3,12 +3,17 @@ import React from 'react'
 const todolist = (props) => {
 
     let listing=[]
+    const completeButtonHandler=(index)=>{
+        console.log(index)
+    }
     props.list.map((value,index)=>{
       
-        let x=<div class="todo"><li class="todo-item" key={index}>{value}</li><button class="complete-btn"><i class="fas fa-check" aria-hidden="true"></i></button><button class="trash-btn"><i class="fas fa-trash" aria-hidden="true"></i></button></div>
+        let x=<div className="todo" key={index}><li className="todo-item" key={index}>{value}</li><button className="complete-btn" onSubmit={completeButtonHandler(index)}><i className="fas fa-check" aria-hidden="true"></i></button><button className="trash-btn"><i className="fas fa-trash" aria-hidden="true"></i></button></div>
         return listing.push(x)
     
     })
+
+   
     
     return (
         <div>
