9b834a73a90c2615ef2c2152517d2d624ae4eab0
Unique Bhattarai <uniqgaming26@gmail.com>
2020-09-07 18:47:31 +0545

    some of the component added to the file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from './Component/Form';
 
 function App() {
   return (
     <div className="App">   
-    
+    <header>
+      Yogesh To Do List(React.js)
+    </header>
+    <Form></Form>
     </div>
   );
 }
---
+++ src/Component/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <form>
+            <input type="text"></input>
+            <button></button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
